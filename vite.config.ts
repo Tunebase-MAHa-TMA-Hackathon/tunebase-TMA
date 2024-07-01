4f2c21381eff3a6f465fb4645ff0868814defc2e
llamiib0t <max774433@gmail.com>
2024-07-01 09:47:56 +0300

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 /// <reference types="vite-plugin-svgr/client" />
 export default defineConfig({
     plugins: [svgr(), react(), basicSsl(), nodePolyfills(), tsconfigPaths()],
-    base: 'https://tunebase.github.io/tunebase-telegram-web-app/',
+    base: 'https://tunebase-maha-tma-hackathon.github.io/tunebase-TMA/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
